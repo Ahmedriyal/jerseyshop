436f6a86cc5bf56c8e64ae20e65a73272e35c404
AhmedRiyal <ahmedriyal@gmail.com>
2022-03-09 16:17:45 +0600

    added database conn

--- eCommerce/eCommerce/settings.py
+++ eCommerce/eCommerce/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+import django_heroku
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-i(va4s&=yqgoo%d(n1scrush!(8e&e5#_jsf&1sq#$uhaf%^^e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["jerseyshop.herokuapp.com", "127.0.0.1"]
 
@@ -81,14 +84,17 @@ WSGI_APPLICATION = "eCommerce.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "df9a0soo03krdt",
-        "USER": "nikvhtqpjzbbbr",
-        "PASSWORD": "365dec5631d8e53a7d4ac7870fa9f254e8699996be1a41f8f75e372198bd1a37",
-        "HOST": "ec2-54-157-160-218.compute-1.amazonaws.com",
-        "PORT": "5432",
+        "NAME": "eCommerce",
+        "USER": "postgres",
+        "PASSWORD": "1234",
+        "HOST": "localhost",
     }
 }
 
+import dj_database_url
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -126,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
