f3265574c5726fcdd6055fe86a5698c9af3a40bd
AhmedRiyal <ahmedriyal@gmail.com>
2023-04-30 10:52:35 +0600

    vercel setting done

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -142,10 +142,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build" , "staticfiles")
+
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build" , "staticfiles")
 
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
